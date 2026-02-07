c44a447fdec23eecc00cc0d0294d907f9dba3201
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2026-02-07 14:51:06 +0500

    feat: implement login page with authentication logic and user interface.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -30,9 +30,9 @@ const Login = () => {
 
             <div className="max-w-md w-full p-8 bg-slate-900/50 backdrop-blur-xl border border-slate-800 rounded-3xl shadow-2xl relative z-10">
                 <div className="text-center mb-10">
-                    <div className="inline-flex items-center justify-center p-4 bg-gray-400 rounded-2xl mb-4 border border-blue-500/30">
-                        {/* <GraduationCap className="w-10 h-10 text-blue-400" /> */}
-                        <img src="/dj-science-college-logo.png" alt="Logo" className="w-10 h-10" />
+                    <div className="w-10 h-10 rounded-xl bg-white inline-flex items-center justify-center">
+                        {/* <GraduationCap className="text-white w-6 h-6" /> */}
+                        <img src="/dj-science-college-logo.png" alt="DJ Science College Logo" className="w-full h-full object-contain" />
                     </div>
                     <h1 className="text-3xl font-bold bg-gradient-to-r from-white to-slate-400 bg-clip-text text-transparent">
                         DJ Science College
